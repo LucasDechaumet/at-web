6f47185c9fe8c26e5bf443f36504ec55fdb39295
LucasAkensys <lucas@akensys.net>
2024-07-30 17:17:37 +0200

    second commit

--- src/app/layout/components/sidebar/sidebar.component.ts
+++ src/app/layout/components/sidebar/sidebar.component.ts
@@ -1,19 +1,22 @@
-import { Component, ElementRef, Renderer2, ViewChild, inject } from '@angular/core';
-import { SidebarService } from './sidebar.service';
-import { ISidebarItem } from './ISidebarItem';
-import { Router, RouterLink, RouterLinkActive } from '@angular/router';
-import { Observable } from 'rxjs';
-import { CommonModule } from '@angular/common';
-
-
-
+import {
+  Component,
+  ElementRef,
+  Renderer2,
+  ViewChild,
+  inject,
+} from "@angular/core";
+import { SidebarService } from "./sidebar.service";
+import { ISidebarItem } from "./ISidebarItem";
+import { Router, RouterLink, RouterLinkActive } from "@angular/router";
+import { Observable } from "rxjs";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-sidebar',
+  selector: "app-sidebar",
   standalone: true,
   imports: [RouterLink, CommonModule, RouterLinkActive],
-  templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss',
+  templateUrl: "./sidebar.component.html",
+  styleUrl: "./sidebar.component.scss",
 })
 export class SidebarComponent {
   public sidebarService: SidebarService = inject(SidebarService);
@@ -21,106 +24,95 @@ export class SidebarComponent {
   public router: Router = inject(Router);
   items: ISidebarItem[] = [
     {
-      "title": "Dashboards",
-      "children": [
+      title: "Dashboards",
+      children: [
         {
-          "title": "Dashboard",
-          "icon": "pi-home",
-          "path": "/dashboard"
+          title: "Expéditions",
+          icon: "pi-box",
+          path: "/expedition",
         },
         {
-          "title": "Users",
-          "icon": "pi-users",
-          "path": "/users"
-        }
-      ]
+          title: "Mouvement",
+          icon: "pi-arrow-right-arrow-left",
+          path: "/mouvement",
+        },
+        {
+          title: "Inventaire",
+          icon: "pi-list-check",
+          path: "/inventaire",
+        },
+        {
+          title: "Remise colis web",
+          icon: "pi-at",
+          path: "/remise-colis-web",
+        },
+        {
+          title: "Enlevement",
+          icon: "pi-cart-minus",
+          path: "/enlevement",
+        },
+        {
+          title: "Réception",
+          icon: "pi-truck",
+          path: "/reception",
+        },
+      ],
     },
     {
-      "title": "Apps",
-      "children": [
+      title: "Compte",
+      children: [
         {
-          "title": "Blog",
-          "icon": "pi-comment",
-          "collapsed": false,
-          "children": [
-            {
-              "title": "Blog",
-              "icon": "pi-comment"
-            },
-            {
-              "title": "Blog",
-              "icon": "pi-comment"
-            },
-            {
-              "title": "Blog",
-              "icon": "pi-comment"
-            }
-          ]
+          title: "Paramètre",
+          icon: "pi-cog",
         },
         {
-          "title": "Calendar",
-          "icon": "pi-calendar"
+          title: "Déconnexion",
+          icon: "pi-sign-out",
         },
-        {
-          "title": "Chat",
-          "icon": "pi-comments"
-        },
-        {
-          "title": "Blog",
-          "icon": "pi-comment",
-          "collapsed": false,
-          "children": [
-            {
-              "title": "Blog",
-              "icon": "pi-comment"
-            },
-            {
-              "title": "Blog",
-              "icon": "pi-comment"
-            },
-            {
-              "title": "Blog",
-              "icon": "pi-comment"
-            }
-          ]
-        }
-      ]
-    }
+      ],
+    },
   ];
   logoPaths = {
-    logoFull: "https://zakariaatattou.github.io/prime-ng-starter-logo/logo.svg",
-    logoSingle: "https://zakariaatattou.github.io/prime-ng-starter-logo/logo-single.svg",
-  }
-  logoPath: string = window.innerWidth > 991 ? this.logoPaths.logoSingle : this.logoPaths.logoFull
+    logoFull:
+      "https://www.familyvillagecostieres-sud.com/-/media/familyvillagecostieressud/images/enseignes/logos/logo_armand_thiery_noir_2_lignes_hd.png?as=0&w=342&hash=6E9D52E72E6037B7F90C5609707F8DC6",
+    logoSingle: "https://cdn.armandthiery.fr/ximg/logoSticky.svg",
+  };
+  logoPath: string =
+    window.innerWidth > 991
+      ? this.logoPaths.logoSingle
+      : this.logoPaths.logoFull;
   constructor(private renderer: Renderer2) {
     /**
      * This events get called by all clicks on the page
      */
-    this.renderer.listen('window', 'click', (e: any) => {
-      if (!document.getElementById('sidebar-toggle')?.contains(e.target) && !document.getElementById('sidebar')?.contains(e.target)) {
+    this.renderer.listen("window", "click", (e: any) => {
+      if (
+        !document.getElementById("sidebar-toggle")?.contains(e.target) &&
+        !document.getElementById("sidebar")?.contains(e.target)
+      ) {
         this.sidebarService.setSidebarState(false);
       }
     });
   }
 
   onToggleSidebar(state: boolean): void {
-    this.sidebarService.setSidebarState(state)
-    this.logoPath = state ? this.logoPaths.logoFull : this.logoPaths.logoSingle
+    this.sidebarService.setSidebarState(state);
+    this.logoPath = state ? this.logoPaths.logoFull : this.logoPaths.logoSingle;
   }
 
   onHoverSidebar(state: boolean) {
     if (!this.sidebarService.getSidebarState) {
-      this.logoPath = state ? this.logoPaths.logoFull : this.logoPaths.logoSingle
+      this.logoPath = state
+        ? this.logoPaths.logoFull
+        : this.logoPaths.logoSingle;
     }
   }
 
   onItemClick(item: ISidebarItem): void {
     if (item?.children?.length) {
-      item.collapsed = !item.collapsed
+      item.collapsed = !item.collapsed;
     } else {
-      this.router.navigateByUrl(item.path as any)
+      this.router.navigateByUrl(item.path as any);
     }
   }
-
-
 }
